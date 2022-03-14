2e8e3892d1d27b140b2b4e08292434b2f60d1ddf
ivan-titovich <i.a.titovich@gmail.com>
2022-03-14 17:03:20 +0300

    hw2_task -1

--- py/collections_hw_2.py
+++ py/collections_hw_2.py
@@ -3,4 +3,8 @@
 ids = {'user1': [213, 213, 213, 15, 213],
        'user2': [54, 54, 119, 119, 119],
        'user3': [213, 98, 98, 35]}
-print(ids)
+# print(ids)
+geos=[]
+for users, geo in ids.items():
+    print(geo)
+
