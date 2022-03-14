4326b47c985beaf216cf8119a7d7f8f610fe3394
Ivan Titovich <i.a.titovich@gmail.com>
2022-03-14 20:44:39 +0300

    h2.py final

--- py/collections_hw_2.py
+++ py/collections_hw_2.py
@@ -4,7 +4,12 @@ ids = {'user1': [213, 213, 213, 15, 213],
        'user2': [54, 54, 119, 119, 119],
        'user3': [213, 98, 98, 35]}
 # print(ids)
-geos=[]
+geos = []
+
 for users, geo in ids.items():
-    print(geo)
+    for i in geo:
+        if i not in geos:
+            geos.append(i)
+print(geos)
+
 
