2d5f3c6658c1ef25eaf7d55b1d2bb0ceb538316e
ivan-titovich <i.a.titovich@gmail.com>
2022-04-05 13:24:27 +0300

    4.collections done

--- py/collections_hw_5.py
+++ py/collections_hw_5.py
@@ -2,28 +2,7 @@
 # {'2018-01-01':{'yandex':{'cpc':100}}}
 l1 = ['2018-01-01', 'yandex', 'cpc', 100]
 
-d2 = {}
-# for i in reversed(l1):
-#     print(i)
-#     d2[i] = i
-#     l1.remove(i)
-#     print(l1)
-#     print(d2)
-#
-# print(d2)
-
-for i in reversed(l1):
-    print(i)
-    d2[i] = l1[len(l1)-1]
-
-print(d2)
-# for item in reversed(l1):
-#     d2[item] = item
-#
-
-
-# new_dict = {}
-# for z in range(1, 21):
-#     new_dict[z] = z ** 2
-#fd
-# print(new_dict)
+d = l1[-1]
+for n in l1[-2::-1]:
+  d = { n: d }
+print(d)
