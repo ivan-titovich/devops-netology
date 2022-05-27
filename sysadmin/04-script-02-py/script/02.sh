cf0e5f302e3457b411eec80dec3e163306a90f2e
Ivan Titovich <i.a.titovich@gmail.com>
2022-05-27 14:43:34 +0300

    [04-script]: Py. 2nd task beggining.

---
+++ sysadmin/04-script-02-py/script/02.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+
+import os
+
+bash_command = ["cd ~/devops-netology/sysadmin", "git status"]
+result_os = os.popen(' && '.join(bash_command)).read()
+print(result_os)
+is_change = False
+for result in result_os.split('\n'):
+	if result.find('modified') != -1:
+        prepare_result = result.replace('\tmodified:   ', '')
+        print(prepare_result)
+        break
+        else
+          print("nothing")
+
+
+
+	
+
