fc211161f44493d216530353094107c226ac57e9
Ivan Titovich <i.a.titovich@gmail.com>
2022-06-02 14:00:56 +0300

    [04-script]: Yaml. 2 task complited.

--- sysadmin/04-script-03-yaml/scripts/02.sh
+++ sysadmin/04-script-03-yaml/scripts/02.sh
@@ -1,12 +1,36 @@
 #!/usr/bin/env python3
-
 import os
+import sys
+import time
+import socket
+import json
+import yaml
 
-bash_command = ["cd ~/devops-netology/sysadmin", "git status"]
-result_os = os.popen(' && '.join(bash_command)).read()
-is_change = False
-path = bash_command[0].replace('cd ', '')
-for result in result_os.split('\n'):
-  if result.find('изменено') != -1:
-    prepare_result = result.replace('\tизменено:      ', '')
-    print(f"{path}/{prepare_result}")
+stop = 0
+
+servers = {'drive.google.com': 0, 'mail.google.com': 0, 'google.com': 0}
+servers['drive.google.com'] = socket.gethostbyname('drive.google.com')
+servers['mail.google.com'] = socket.gethostbyname('mail.google.com')
+servers['google.com'] = socket.gethostbyname('google.com')
+
+
+while stop != 1 :
+  for k,v in servers.items():
+    time.sleep(0.2)
+    if  socket.gethostbyname(k) == str(v) :
+      old_ip = v
+      print(f"<{k}> - <{v}>")
+      servers[k] = v
+    else :
+      stop = 1
+      print(f"[ERROR] <{k}> IP mismatch: <{old_ip}> <{v}>")
+      servers[k] = v
+
+with open('services.json', 'w') as f:
+  json.dump(servers, f, sort_keys=True, indent=2)
+with open('services.yaml', 'w') as file:
+  yaml.dump(servers, file)
+
+
+
+
--- sysadmin/04-script-03-yaml/scripts/03.sh
+++ sysadmin/04-script-03-yaml/scripts/03.sh
@@ -1,28 +1,5 @@
 #!/usr/bin/env python3
 import os
 import sys
-
-argument = None
-
-bash_command = ["cd ~/devops-netology/sysadmin", "git status"]
-
-path = bash_command[0].replace('cd ', '')
-
-if len(sys.argv) > 1 :
-  argument = sys.argv[1]
-  if os.path.exists(argument+'.git'):
-    bash_command[0] = 'cd '+argument
-    result_os = os.popen(' && '.join(bash_command)).read()
-    for result in result_os.split('\n'):
-      if result.find('изменено') != -1:
-        prepare_result = result.replace('\tизменено:      ', '')
-        print(f"{path}/{prepare_result}")
-  else:
-    print('Репозитория гит в папке не существует')
-else:
-  print('Аргумент не задан - ищем по пути: ', path)
-  result_os = os.popen(' && '.join(bash_command)).read()
-  for result in result_os.split('\n'):
-    if result.find('изменено') != -1:
-      prepare_result = result.replace('\tизменено:      ', '')
-      print(f"{path}/{prepare_result}")
+import time
+import socket
