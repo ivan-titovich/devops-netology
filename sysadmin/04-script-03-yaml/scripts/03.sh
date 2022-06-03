d682098904e5afc173cf52d66cadded4ceaec0d5
Ivan Titovich <i.a.titovich@gmail.com>
2022-06-03 09:41:49 +0300

    [04-script]: Yaml. 3 task completed.

--- sysadmin/04-script-03-yaml/scripts/03.sh
+++ sysadmin/04-script-03-yaml/scripts/03.sh
@@ -10,50 +10,41 @@ import yaml
 regex_json = ".json$"
 regex_yaml = '.(yml)|(yaml)$'
 regex_json_ident =  '^{'
-
+regex_yaml_ident =  '^(?!{)'
+jf = None
+yf = None
 
 if len(sys.argv) > 1:
   input_filename = sys.argv[1]
-  print(input_filename)
   if re.search( regex_json, input_filename) :
-    print('json file is: ' + input_filename )
-    with open(input_filename) as json_file:
-      json_data = json.load(json_file)
-      print(type(json_data))
-      print(json.dumps(json_data, indent=2))
-#      if re.findall( regex_json_ident, json_data[0]) :
-#        print('This is json-file')
+    filename_w_e = re.sub(regex_json, '', input_filename)
+    try :
+      with open(input_filename) as json_file:
+        json_data = json.load(json_file)
+        with open(sys.argv[1]) as file:
+          for line in file:
+            if re.match( regex_json_ident, line) :
+              jf = True
+              with open(filename_w_e + '.yaml', 'w') as file:
+                yaml.dump(json_data, file)
+        print(json.dumps(json_data, indent=2))
+    except json.decoder.JSONDecodeError as error:
+      print("[JSON validation ERROR] " + str(error))
+
   elif re.search( regex_yaml, input_filename) :
-    print('yaml file is: ' + input_filename )
-    with open(input_filename) as yaml_file:
-      yaml_data = yaml.safe_load(yaml_file)
-      print(type(yaml_data))
-      print(yaml.dump(yaml_data, indent=2))
+    filename_w_e = re.sub(regex_yaml, '', input_filename)
+    try:
+      with open(input_filename) as yaml_file:
+        yaml_data = yaml.safe_load(yaml_file)
+        with open(sys.argv[1]) as file:
+          for line in file:
+            if re.match( regex_yaml_ident, line) :
+              yf = True
+              with open(filename_w_e + '.json', 'w') as file:
+                json.dump(yaml_data, file)
+        print(yaml.dump(yaml_data, indent=2))
+    except yaml.parser.ParserError as error:
+      print("[YAML validation ERROR] " + str(error))
   else :
     print("Error: this is not json or yaml file!")
     sys.exit()
-
-
-
-#bash_command = ["cd ~/devops-netology/sysadmin", "git status"]
-#
-#path = bash_command[0].replace('cd ', '')
-#
-#if len(sys.argv) > 1 :
-#  argument = sys.argv[1]
-#  if os.path.exists(argument+'.git'):
-#    bash_command[0] = 'cd '+argument
-#    result_os = os.popen(' && '.join(bash_command)).read()
-#    for result in result_os.split('\n'):
-#      if result.find('изменено') != -1:
-#        prepare_result = result.replace('\tизменено:      ', '')
-#        print(f"{path}/{prepare_result}")
-#  else:
-#    print('Репозитория гит в папке не существует')
-#else:
-#  print('Аргумент не задан - ищем по пути: ', path)
-#  result_os = os.popen(' && '.join(bash_command)).read()
-#  for result in result_os.split('\n'):
-#    if result.find('изменено') != -1:
-#      prepare_result = result.replace('\tизменено:      ', '')
-#      print(f"{path}/{prepare_result}")
