3367a90a2df972c1985a2418346005ec6f85980d
Ivan Titovich <i.a.titovich@gmail.com>
2022-07-06 11:37:47 +0300

    [05-virt-04-docker-compose]: 1-4 done.

--- sysadmin/05-virt-04-docker-compose/src/terraform/variables.tf
+++ sysadmin/05-virt-04-docker-compose/src/terraform/variables.tf
@@ -13,5 +13,5 @@ variable "yandex_folder_id" {
 # Заменить на ID своего образа
 # ID можно узнать с помощью команды yc compute image list
 variable "centos-7-base" {
-  default = "fd8pehsv5i0krequ1ncc"
+  default = "fd8cbku28uqr593n6067"
 }
