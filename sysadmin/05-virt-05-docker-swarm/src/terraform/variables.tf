b0579d6e509a3d0cd5ed1297f9c59e4dfd8dd3bb
Ivan Titovich <i.a.titovich@gmail.com>
2022-07-07 08:31:11 +0300

    [05-virt-04-docker-swarm]: 1-4 done.

---
+++ sysadmin/05-virt-05-docker-swarm/src/terraform/variables.tf
@@ -0,0 +1,17 @@
+# Заменить на ID своего облака
+# https://console.cloud.yandex.ru/cloud?section=overview
+variable "yandex_cloud_id" {
+  default = "b1g2pk80ee8v616b595i"
+}
+
+# Заменить на Folder своего облака
+# https://console.cloud.yandex.ru/cloud?section=overview
+variable "yandex_folder_id" {
+  default = "b1g488ps1dq37s4sps0v"
+}
+
+# Заменить на ID своего образа
+# ID можно узнать с помощью команды yc compute image list
+variable "centos-7-base" {
+  default = "fd8buseqvg8hjgqs2s8a"
+}
